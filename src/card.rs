42ed5bff8695036c6213fe0a778389ad1bc3d0de
TylerBloom <tylerbloom2222@gmail.com>
2021-09-23 17:10:29 -0400

    Added the card database.

--- src/card.rs
+++ src/card.rs
@@ -1,19 +1,11 @@
 
 pub mod Card{
 
-    use uuid::Uuid;
-    
     pub struct Card {
-        id: Uuid,
-        name: String,
+        pub name: String,
+        pub printings: Vec<String>,
+        pub types: Vec<String>
     }
 
-    impl Card {
-        pub fn hello( &self ) -> String { "Hello World".to_string() }
-    }
-
-    pub fn NewCard( name: String ) -> Card {
-        Card { name, id: Uuid::new_v4() }
-    }
 }
 
