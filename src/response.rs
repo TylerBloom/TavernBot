1721108aa6f58aea6d28580c8cd872f01ebb2cf1
TylerBloom <tylerbloom2222@gmail.com>
2021-10-30 23:37:53 -0400

    Working on response struct.

--- src/response.rs
+++ src/response.rs
@@ -1,14 +1,14 @@
 pub mod Response {
-    
+
     use std::collections::HashMap;
 
-    use serenity::prelude::*;
-    use serenity::model::channel::Embed;
-    use serenity::framework::standard::CommandResult;
-    use serenity::model::channel::Message;
     use serenity::builder::CreateMessage;
+    use serenity::framework::standard::CommandResult;
+    use serenity::model::channel::Embed;
+    use serenity::model::channel::Message;
+    use serenity::prelude::*;
     use serenity::utils::Colour;
-    
+
     // Models a serenity Embed.  Unlike in discord.py, embeds can't really be
     // created, modified, and then sent.  The send_message method takes a
     // FnOnce of a CreateEmbed, so they have to be created when they are sent.
@@ -22,7 +22,7 @@ pub mod Response {
         pub colour: Colour,
         pub fields: Vec<(String, String, bool)>,
     }
-    
+
     pub struct Response {
         content: Option<String>,
         embed: Option<EmbedSpoof>,
@@ -30,44 +30,49 @@ pub mod Response {
 
     pub fn new() -> Response {
         Response {
-            content: None, embed: None,
+            content: None,
+            embed: None,
         }
     }
-    
-    pub async fn send_message( res: Response, ctx: &Context, msg: &Message ) -> CommandResult {
-        if( res.content.is_some() && res.embed.is_some() ) {
+
+    pub async fn send_message(res: Response, ctx: &Context, msg: &Message) -> CommandResult {
+        if (res.content.is_some() && res.embed.is_some()) {
             // The message contains both content and an embed
             let embed = res.embed.unwrap().clone();
-            msg.channel_id.send_message(&ctx.http, |m| {
-                m.embed( |mut e| {
-                    e.title( embed.title );
-                    e.colour( embed.colour );
-                    e.fields( embed.fields );
-                    
-                    e
-                } );
-            
-                m
-            }).await?;
+            msg.channel_id
+                .send_message(&ctx.http, |m| {
+                    m.embed(|mut e| {
+                        e.title(embed.title);
+                        e.colour(embed.colour);
+                        e.fields(embed.fields);
+
+                        e
+                    });
+
+                    m
+                })
+                .await?;
             Ok(())
-        } else if( res.content.is_some() ) {
+        } else if (res.content.is_some()) {
             // The response only contains some content
-            msg.channel_id.say( &ctx.http, &res.content.unwrap() ).await?;
+            msg.channel_id.say(&ctx.http, &res.content.unwrap()).await?;
             Ok(())
-        } else if( res.embed.is_some() ) {
+        } else if (res.embed.is_some()) {
             // The response only contains an embed
             let embed = res.embed.unwrap().clone();
-            msg.channel_id.send_message(&ctx.http, |m| {
-                m.embed( |mut e| {
-                    e.title( embed.title );
-                    e.colour( embed.colour );
-                    e.fields( embed.fields );
-                    
-                    e
-                } );
-            
-                m
-            }).await?;
+            msg.channel_id
+                .send_message(&ctx.http, |m| {
+                    m.embed(|mut e| {
+                        e.title(embed.title);
+                        e.colour(embed.colour);
+                        e.fields(embed.fields);
+
+                        e
+                    });
+
+                    m
+                })
+                .await?;
             Ok(())
         } else {
             // There is neither content nor an embed, so we will exit quietly
@@ -76,23 +81,23 @@ pub mod Response {
     }
 
     impl Response {
-        pub fn content( &self ) -> String {
+        pub fn content(&self) -> String {
             match self.content.clone() {
                 None => String::from(""),
                 Some(c) => c.clone(),
             }
         }
-        pub fn set_content( &mut self, msg: String ) {
+        pub fn set_content(&mut self, msg: String) {
             self.content = Some(msg);
         }
-        
-        pub fn embed_title( &self ) -> String {
+
+        pub fn embed_title(&self) -> String {
             match self.embed.clone() {
                 None => String::from(""),
                 Some(e) => e.title,
             }
         }
-        pub fn set_embed( &mut self, embed: EmbedSpoof ) {
+        pub fn set_embed(&mut self, embed: EmbedSpoof) {
             self.embed = Some(embed);
         }
     }
